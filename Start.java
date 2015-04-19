9609e65c840f7b3ed72a40cd6d79bb4aea91c783
Pawel Szczurko <pszczurko@gmail.com>
2015-04-18 22:58:48 -0400

    Cleanup Start point file

--- Start.java
+++ Start.java
@@ -1,13 +1,13 @@
 public class Start {
     public static void main(String [] args) {
         try {
-            System.out.println("hello");
+            System.out.println("Starting SOCKET SERVER....");
             MyWebSocketServer.main(new String[0]);
-            System.out.println("world!!!!");
+            System.out.println("SOCKET SERVER running\n\nStarting LOCAL WEB SERVER.....");
             RunServer.main(new String[0]);
-            System.out.println("All servers running!");
+            System.out.println("LOCAL WEB SERVER running\nAll servers running!");
         } catch (Exception e) {
-            System.out.println("Could not start Socket Server");
+            System.out.println("Could not start servers");
         }
 
     }
